5360fa0e3284951cf74161e6303d11b00c3d9b41
Andrew Darqui <andrew.darqui@gmail.com>
2016-07-17 03:43:49 -0400

    slight refactor using request/response types, more derived classes, strict record fields

--- src/Web/TodoMVC/Backend/Pure/Todo/Types.hs
+++ src/Web/TodoMVC/Backend/Pure/Todo/Types.hs
@@ -1,87 +1,113 @@
+{-# LANGUAGE DeriveAnyClass    #-}
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards   #-}
 {-# LANGUAGE TemplateHaskell   #-}
 
 module Web.TodoMVC.Backend.Pure.Todo.Types (
-  Todo (..),
+  TodoRequest (..),
+  TodoResponse (..),
+  TodoResponses,
   TodoActionRequest (..),
   TodoActionResponse (..),
   TodoState (..),
   TodoApp (..),
   TodoAppState,
   TodoId,
-  todoId,
-  todoTitle,
-  todoState,
+  todoRequestTitle,
+  todoResponseId,
+  todoResponseTitle,
+  todoResponseState,
+  todoResponseCreatedAt,
+  todoResponseModifiedAt,
   todoAppCounter,
   todoAppTodos
 ) where
 
 
 
+import           Control.DeepSeq     (NFData)
 import           Control.Lens        (makeLenses)
 import           Control.Monad.State (State)
 import           Data.Aeson          (FromJSON, ToJSON)
-import qualified Data.Map            as M (Map)
+import           Data.Int            (Int64)
+import           Data.Map            (Map)
 import           Data.Text           (Text)
+import           Data.Time           (UTCTime)
+import           Data.Typeable       (Typeable)
 import           GHC.Generics        (Generic)
 
 
 
-type TodoId = Int
+type TodoId = Int64
 
 
 
 data TodoState
   = Active
+  | Editing
   | Completed
-  deriving (Show, Eq, Ord, Generic)
+  deriving (Show, Eq, Ord, Generic, Typeable, NFData)
 
 instance FromJSON TodoState
 instance ToJSON TodoState
 
 
 
-data Todo = Todo {
-  _todoId    :: TodoId,
-  _todoTitle :: Text,
-  _todoState :: TodoState
-} deriving (Show, Eq, Ord, Generic)
+data TodoRequest = TodoRequest {
+  _todoRequestTitle :: !Text
+} deriving (Show, Eq, Ord, Generic, Typeable, NFData)
 
-makeLenses ''Todo
+makeLenses ''TodoRequest
 
-instance FromJSON Todo
-instance ToJSON Todo
+instance FromJSON TodoRequest
+instance ToJSON TodoRequest
+
+
+
+data TodoResponse = TodoResponse {
+  _todoResponseId         :: !TodoId,
+  _todoResponseTitle      :: !Text,
+  _todoResponseState      :: !TodoState,
+  _todoResponseCreatedAt  :: !(Maybe UTCTime),
+  _todoResponseModifiedAt :: !(Maybe UTCTime)
+} deriving (Show, Eq, Ord, Generic, Typeable, NFData)
+
+makeLenses ''TodoResponse
+
+instance FromJSON TodoResponse
+instance ToJSON TodoResponse
+
+type TodoResponses = [TodoResponse]
 
 
 
 data TodoActionRequest
   = ReqListTodos
-  | ReqAddTodo      Todo
+  | ReqAddTodo      TodoRequest
   | ReqRemoveTodo   TodoId
-  | ReqUpdateTodo   TodoId Todo
+  | ReqUpdateTodo   TodoId TodoRequest
   | ReqFindTodoById TodoId
   | ReqClearTodos
-  deriving (Show, Eq, Ord, Generic)
+  deriving (Show, Eq, Ord, Generic, Typeable, NFData)
 
 
 
 data TodoActionResponse
-  = RespListTodos    [Todo]
-  | RespAddTodo      (Maybe Todo)
+  = RespListTodos    TodoResponses
+  | RespAddTodo      (Maybe TodoResponse)
   | RespRemoveTodo   (Maybe TodoId)
-  | RespUpdateTodo   (Maybe Todo)
-  | RespFindTodoById (Maybe Todo)
+  | RespUpdateTodo   (Maybe TodoResponse)
+  | RespFindTodoById (Maybe TodoResponse)
   | RespClearTodos   Bool
-  deriving (Show, Eq, Ord, Generic)
+  deriving (Show, Eq, Ord, Generic, Typeable, NFData)
 
 
 
 data TodoApp = TodoApp {
-  _todoAppTodos   :: M.Map TodoId Todo,
-  _todoAppCounter :: TodoId
-} deriving (Show, Eq, Ord, Generic)
+  _todoAppTodos   :: !(Map TodoId TodoResponse),
+  _todoAppCounter :: !TodoId
+} deriving (Show, Eq, Ord, Generic, Typeable, NFData)
 
 makeLenses ''TodoApp
 
